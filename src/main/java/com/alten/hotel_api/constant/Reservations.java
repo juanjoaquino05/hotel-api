b3dcc52f0136aa2e2a51d1b046edc46dd0b8dcda
Juan Henriquez <juanjoaquino05@gmail.com>
2022-09-26 11:19:58 -0400

    Adding max reservation days value

--- src/main/java/com/alten/hotel_api/constant/Reservations.java
+++ src/main/java/com/alten/hotel_api/constant/Reservations.java
@@ -3,4 +3,5 @@ package com.alten.hotel_api.constant;
 public class Reservations {
     public static final Integer MAX_DAYS_IN_ADVANCE = 30;
     public static final Integer MIN_RESERVATION_START = 1;
+    public static final Integer MAX_RESERVATION_DAYS = 3;
 }
