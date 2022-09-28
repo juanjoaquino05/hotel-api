b1e8f95b07cea0045da0252f197fa700cb924709
Juan Henriquez <juanjoaquino05@gmail.com>
2022-09-27 22:48:20 -0400

    Adding Reservation Tests

--- src/main/java/com/alten/hotel_api/controller/ReservationController.java
+++ src/main/java/com/alten/hotel_api/controller/ReservationController.java
@@ -1,11 +1,9 @@
 package com.alten.hotel_api.controller;
 
 import com.alten.hotel_api.model.Reservation;
-import com.alten.hotel_api.model.Room;
 import com.alten.hotel_api.request.CreateReservationRequest;
 import com.alten.hotel_api.response.CreateReservationResponse;
 import com.alten.hotel_api.service.ReservationService;
-import com.alten.hotel_api.service.RoomService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +12,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.time.LocalDate;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/reservations")
@@ -39,7 +35,7 @@ public class ReservationController {
     }
 
     @PutMapping("/{rid}/user/{id}")
-    public ResponseEntity<CreateReservationResponse> replaceEmployee(@PathVariable Long id, @PathVariable Long rid, @RequestBody Reservation reservation) throws Exception {
+    public ResponseEntity<CreateReservationResponse> modifyReservation(@PathVariable Long id, @PathVariable Long rid, @RequestBody Reservation reservation) throws Exception {
 
         return service.modifyReservation(id, rid, reservation);
 
